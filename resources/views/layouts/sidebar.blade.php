d81276d70ca114a4cae4dd113b7468e82098fbcd
Mahfudz Khoirun Nizam <mahfudzkhoirunnizam@gmail.com>
2024-03-19 23:03:42 +0700

    admin theme

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,28 @@
+<aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+    <div class="app-brand demo">
+        <a href="index.html" class="app-brand-link">
+            <span class="app-brand-logo demo">
+                <img src="{{asset('assets/img/icon.png')}}" alt="" width="25" height="25" class="my-3" >
+            </span>
+            <span class="app-brand-text demo menu-text fw-bolder ms-2">Space Health</span>
+        </a>
+
+        <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
+            <i class="bx bx-chevron-left bx-sm align-middle"></i>
+        </a>
+    </div>
+
+    <div class="menu-inner-shadow"></div>
+
+    <ul class="menu-inner py-1">
+        <!-- Dashboard -->
+        <li class="menu-item active">
+            <a href="index.html" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                <div data-i18n="Analytics">Dashboard</div>
+            </a>
+        </li>
+
+        
+    </ul>
+</aside>
